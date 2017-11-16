bfc5d23375906124ec8688aec172dcde58cc202c
Anton Babenko <anton@antonbabenko.com>
2017-11-16 16:51:09 +0100

    Fixed outputs when security group is not created

--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,34 @@
 output "this_security_group_id" {
   description = "The ID of the security group"
-  value       = "${aws_security_group.this.id}"
+  value       = "${element(concat(aws_security_group.this.*.id, list("")), 0)}"
 }
 
 output "this_security_group_vpc_id" {
   description = "The VPC ID"
-  value       = "${aws_security_group.this.vpc_id}"
+  value       = "${element(concat(aws_security_group.this.*.vpc_id, list("")), 0)}"
 }
 
 output "this_security_group_owner_id" {
   description = "The owner ID"
-  value       = "${aws_security_group.this.owner_id}"
+  value       = "${element(concat(aws_security_group.this.*.owner_id, list("")), 0)}"
 }
 
 output "this_security_group_name" {
   description = "The name of the security group"
-  value       = "${aws_security_group.this.name}"
+  value       = "${element(concat(aws_security_group.this.*.name, list("")), 0)}"
 }
 
 output "this_security_group_description" {
   description = "The description of the security group"
-  value       = "${aws_security_group.this.description}"
+  value       = "${element(concat(aws_security_group.this.*.description, list("")), 0)}"
 }
 
 output "this_security_group_ingress" {
   description = "The ingress rules"
-  value       = "${aws_security_group.this.ingress}"
+  value       = "${element(concat(aws_security_group.this.*.ingress, list("")), 0)}"
 }
 
 output "this_security_group_egress" {
   description = "The egress rules"
-  value       = "${aws_security_group.this.egress}"
+  value       = "${element(concat(aws_security_group.this.*.egress, list("")), 0)}"
 }
