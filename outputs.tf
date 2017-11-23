b2a770383b0d7bf7cda34e39a792f90885ca0c66
Brandon J. O'Connor <brandoconnor@gmail.com>
2017-11-23 01:34:07 -0800

    formatting to help downstream depedent modules pass when running CI (#22)

--- outputs.tf
+++ outputs.tf
@@ -28,7 +28,9 @@ output "this_security_group_description" {
 //  value       = "${element(concat(aws_security_group.this.*.ingress, list("")), 0)}"
 //}
 
+
 //output "this_security_group_egress" {
 //  description = "The egress rules"
 //    value       = "${element(concat(aws_security_group.this.*.egress, list("")), 0)"
 //}
+
