c8c0f84b954176cff53cc0f8b0a3bccf9a789953
Nazar Annagurban <nazartm@gmail.com>
2018-01-16 04:07:44 -0500

    Workaround for bug when updating description of a rule with protocol all
    Error:
    
    "Error updating security group rule description:
    InvalidParameterValue: When protocol is ALL, you cannot specify
    from-port."
    
    Issue:
    https://github.com/terraform-providers/terraform-provider-aws/issues/1920

--- main.tf
+++ main.tf
@@ -111,6 +111,10 @@ resource "aws_security_group_rule" "ingress_with_self" {
 resource "aws_security_group_rule" "egress_rules" {
   count = "${var.create ? length(var.egress_rules) : 0}"
 
+  lifecycle {
+    "ignore_changes" =  ["description"]
+  }
+
   security_group_id = "${aws_security_group.this.id}"
   type              = "egress"
 
@@ -118,7 +122,7 @@ resource "aws_security_group_rule" "egress_rules" {
   ipv6_cidr_blocks = ["${var.egress_ipv6_cidr_blocks}"]
   prefix_list_ids  = ["${var.egress_prefix_list_ids}"]
   description      = "${element(var.rules[var.egress_rules[count.index]], 3)}"
-  
+
 
   from_port = "${element(var.rules[var.egress_rules[count.index]], 0)}"
   to_port   = "${element(var.rules[var.egress_rules[count.index]], 1)}"
@@ -149,6 +153,10 @@ resource "aws_security_group_rule" "egress_with_source_security_group_id" {
 resource "aws_security_group_rule" "egress_with_cidr_blocks" {
   count = "${var.create ? length(var.egress_with_cidr_blocks) : 0}"
 
+  lifecycle {
+    "ignore_changes" =  ["description"]
+  }
+
   security_group_id = "${aws_security_group.this.id}"
   type              = "egress"
 
@@ -164,6 +172,10 @@ resource "aws_security_group_rule" "egress_with_cidr_blocks" {
 # Security group rules with "ipv6_cidr_blocks", but without "cidr_blocks", "source_security_group_id" and "self"
 resource "aws_security_group_rule" "egress_with_ipv6_cidr_blocks" {
   count = "${var.create ? length(var.egress_with_ipv6_cidr_blocks) : 0}"
+
+  lifecycle {
+    "ignore_changes" =  ["description"]
+  }
 
   security_group_id = "${aws_security_group.this.id}"
   type              = "egress"
