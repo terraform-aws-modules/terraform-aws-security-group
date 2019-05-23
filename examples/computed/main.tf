bb5a855b18d164c79de15f2f0074ea7fec3187de
Alex Goncharov <alex@goncharov.co>
2019-05-23 21:21:40 +0200

    update examples for 0.12

--- examples/computed/main.tf
+++ examples/computed/main.tf
@@ -11,7 +11,7 @@ data "aws_vpc" "default" {
 
 data "aws_security_group" "default" {
   name   = "default"
-  vpc_id = "${data.aws_vpc.default.id}"
+  vpc_id = data.aws_vpc.default.id
 }
 
 ###########################
@@ -22,14 +22,14 @@ module "http_sg" {
 
   name        = "computed-http-sg"
   description = "Security group with HTTP port open for everyone, and HTTPS open just for the default security group"
-  vpc_id      = "${data.aws_vpc.default.id}"
+  vpc_id      = data.aws_vpc.default.id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   ingress_with_source_security_group_id = [
     {
       rule                     = "https-443-tcp"
-      source_security_group_id = "${data.aws_security_group.default.id}"
+      source_security_group_id = data.aws_security_group.default.id
     },
   ]
 }
@@ -39,16 +39,17 @@ module "mysql_sg" {
 
   name        = "computed-mysql-sg"
   description = "Security group with MySQL/Aurora port open for HTTP security group created above (computed)"
-  vpc_id      = "${data.aws_vpc.default.id}"
+  vpc_id      = data.aws_vpc.default.id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   computed_ingress_with_source_security_group_id = [
     {
       rule                     = "mysql-tcp"
-      source_security_group_id = "${module.http_sg.this_security_group_id}"
+      source_security_group_id = module.http_sg.this_security_group_id
     },
   ]
 
   number_of_computed_ingress_with_source_security_group_id = 1
 }
+
