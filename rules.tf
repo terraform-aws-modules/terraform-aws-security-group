63153ced6b0da224282d4e67a8ad45ad35253f19
Mariano Paz <paz.mariano@gmail.com>
2019-10-28 10:11:52 -0300

    New rules tcp for custom ports: 8081, 8083

--- rules.tf
+++ rules.tf
@@ -121,6 +121,10 @@ variable "rules" {
     # Redshift
     redshift-tcp = [5439, 5439, "tcp", "Redshift"]
 
+    # Custom TCP
+    sisense-https-8081 = [8081,8081, "tcp", "Custom-HTTPS-Sisense-8081"]
+    sisense-https-8083 = [8083,8083, "tcp", "Custom-HTTPS-Sisense-8083"]
+
     # Splunk
     splunk-indexer-tcp = [9997, 9997, "tcp", "Splunk indexer"]
     splunk-clients-tcp = [8080, 8080, "tcp", "Splunk clients"]
@@ -337,7 +341,13 @@ variable "auto_groups" {
       ingress_with_self = ["all-all"]
       egress_rules      = ["all-all"]
     }
-
+    
+    sisense = {
+        ingress_rules   = ["sisense-https-8081", "sisense-https-8083"]
+        ingress_with_self = ["all-all"]
+        egress_rules      = ["all-all"]
+    }
+    
     splunk = {
       ingress_rules     = ["splunk-indexer-tcp", "splunk-clients-tcp", "splunk-splunkd-tcp", "splunk-hec-tcp"]
       ingress_with_self = ["all-all"]
