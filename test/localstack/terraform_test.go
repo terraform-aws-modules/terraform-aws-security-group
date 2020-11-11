f5f710a1bbf2d3b513a25f1853efcd0319639308
Sephone Slattery <lynnreave@gmail.com>
2020-11-11 10:42:41 -0800

    Updated unit tests.

--- test/localstack/terraform_test.go
+++ test/localstack/terraform_test.go
@@ -7,59 +7,52 @@ import (
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/aws"
 )
 
 func TestTerraform(t *testing.T) {
-	// get localstack host
-	aws_host := os.Getenv("AWS_HOST")
-	if aws_host == "" {
-		aws_host = "localhost"
-	}
-	fmt.Println("Expecting localstack on", aws_host)
+		// get localstack host
+		aws_host := os.Getenv("AWS_HOST")
+		if aws_host == "" {
+			aws_host = "localhost"
+		}
+		fmt.Println("Expecting localstack on", aws_host)
 
-	// configure the test
-	terraformOptions := &terraform.Options{
-		// set the path to the terraform code that will be tested
-		TerraformDir: "./sg",
-		// set variables; set localstack host var
-		Vars:  map[string]interface{} {
-			"aws_host": aws_host,
-		},
+		// configure the test
+		terraformOptions := &terraform.Options{
+			// set the path to the terraform code that will be tested
+			TerraformDir: "./sg",
+			// set variables; set localstack host var
+			Vars:  map[string]interface{} {
+				"aws_host": aws_host,
+			},
 
-		// Environment variables to set when running Terraform
-		EnvVars: map[string]string{
-			"AWS_DEFAULT_REGION": "us-east-1",
-		},
-	}
+			// Environment variables to set when running Terraform
+			EnvVars: map[string]string{
+				"AWS_DEFAULT_REGION": "us-east-1",
+			},
+		}
 
-	defer terraform.Destroy(t, terraformOptions)
-	terraform.InitAndApply(t, terraformOptions)
+		// configure to clean up resources with `terraform destroy` at the end of the test
+		defer terraform.Destroy(t, terraformOptions)
 
-    // AWS
+		// BUILD VERIFICATION TEST (attempt to run `terraform init` and `terraform apply`)
+		terraform.InitAndApply(t, terraformOptions)
 
-    // Security Group
-    security_group_name := terraform.Output(t, terraformOptions, "this_security_group_name")
-    var security_group_names []string
-
-    svc_ec2 := ec2.New(session.New(&aws.Config{
-        Region: aws.String("us-east-1"),
-    }))
-    input_sg := &ec2.DescribeSecurityGroupsInput{}
-    err_sg := svc_ec2.DescribeSecurityGroupsPages(input_sg,
-        func(page *ec2.DescribeSecurityGroupsOutput, lastPage bool) bool {
-            for _, v := range page.SecurityGroups {
-                security_group_names = append(security_group_names, aws.StringValue(v.GroupName))
-            }
-            return lastPage
-        },
-    )
-    if err_sg != nil {
-        fmt.Println(err_sg.Error())
-        return
-    }
-
-    assert.Contains(t, security_group_names, security_group_name)
+		// UNIT TESTS
+		// run `terraform output` and verify values
+		this_security_group_id := terraform.Output(t, terraformOptions, "this_security_group_id")
+		fmt.Println(this_security_group_id)
+		assert.NotNil(t, this_security_group_id)
+		this_security_group_vpc_id := terraform.Output(t, terraformOptions, "this_security_group_vpc_id")
+		fmt.Println(this_security_group_vpc_id)
+		assert.NotNil(t, this_security_group_vpc_id)
+		this_security_group_owner_id := terraform.Output(t, terraformOptions, "this_security_group_owner_id")
+		fmt.Println(this_security_group_owner_id)
+		assert.NotNil(t, this_security_group_owner_id)
+		this_security_group_name := terraform.Output(t, terraformOptions, "this_security_group_name")
+		fmt.Println(this_security_group_name)
+		assert.Equal(t, "securitygroup-test", this_security_group_name)
+		this_security_group_description := terraform.Output(t, terraformOptions, "this_security_group_description")
+		fmt.Println(this_security_group_description)
+		assert.Equal(t, "Security Group managed by Terraform", this_security_group_description)
 }
